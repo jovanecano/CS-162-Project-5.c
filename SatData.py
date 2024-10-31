6b5a037bf19f759138bd11040be8cd112c95ced5
jovanecano <jovanecano1995@gmail.com>
2024-10-30 19:14:23 -0700

    started the satdata class

---
+++ SatData.py
@@ -0,0 +1,12 @@
+# Author: Jovane Cano
+# GitHub Username: jovanecano
+# Date: 10/30/2024
+# Description:
+""" program contains a class named SatData that reads a json file
+ and writes the data to a text file in a csv format"""
+
+#importing the json module
+import json
+
+class SatData:
+    def __init__(self):
